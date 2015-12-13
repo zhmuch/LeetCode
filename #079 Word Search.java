3cdfda1755c997ff0eef0a6a92cb9ee519e3e710
zhmuch <zhmuch@users.noreply.github.com>
2015-12-13 15:26:57 -0500

    Create #079 Word Search.java

---
+++ #079 Word Search.java
@@ -0,0 +1,17 @@
+public class Solution {
+	public boolean exist(char[][] board, String word){
+		boolean[][] flag;
+	 
+	}
+	public search(char[][] board, int i, int j, char t){
+		switch(t){
+			case board[cow-1,row]:
+				break;
+			case board[cow, row-1]:
+				break;
+
+				
+		
+		}
+	} 
+}
