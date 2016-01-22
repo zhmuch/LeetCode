499a79276f513303a836566474d918aad6462254
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-01-22 01:24:02 -0500

    Update #136 Single Number.java

--- #136 Single Number.java
+++ #136 Single Number.java
@@ -1,4 +1,16 @@
-
+/*
+    ^ : 异或。
+    N1 ^ N2 ^ N3 ^...=(Ni ^ Nj) ^ ...  任意i，j。
+*/
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for(int i=1; i<nums.length; i++){
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
 
 // Very poor.
 
