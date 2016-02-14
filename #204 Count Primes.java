410d76f494c08938b8ee46e17691bdb5ba9dbb30
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-02-13 23:21:52 -0500

    Update #204 Count Primes.java

--- #204 Count Primes.java
+++ #204 Count Primes.java
@@ -1,4 +1,30 @@
 /*
+  筛法求质数。
+*/
+import java.util.Arrays;
+
+public class Solution {
+    public int countPrimes (int n) {
+        if(n<=2) return 0;
+
+        boolean[] pri = new boolean[n+1];
+        
+        for(int i=2; i*i<n; i++){
+            if(!pri[i]){
+                for(int j=i; i*j<n; j++){
+                    pri[i*j] = true;
+                }
+            }
+        }
+        int count = 0;
+        for(int i=2; i<n; i++){
+            if(!pri[i]) count++;
+        }
+        return count;
+    }
+}
+
+/*
   Time exceed, big number.
 */
 
