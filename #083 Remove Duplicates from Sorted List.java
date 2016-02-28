6d2ca6a2c022db5f3eea6c992ded054cfc36ccff
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-02-28 01:47:56 -0500

    Create #083 Remove Duplicates from Sorted List.java

---
+++ #083 Remove Duplicates from Sorted List.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null)
+            return head;
+
+        ListNode curr = head;
+        while(curr.next != null){
+            if(curr.val == curr.next.val)
+                curr.next = curr.next.next;
+            else
+                curr = curr.next;
+        }
+        return head;
+    }
+}
