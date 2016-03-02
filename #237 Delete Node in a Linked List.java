205657de5b9fb9cc057d1a4700d03791def622da
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-03-01 21:54:23 -0500

    Create #237 Delete Node in a Linked List.java

---
+++ #237 Delete Node in a Linked List.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+        while(node.next != null){
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        prev.next = null;
+        
+        return;
+    }
+}
