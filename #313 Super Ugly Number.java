1b623b5e59373715a68a4c99a7658e13ef217475
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-03-05 02:10:53 -0500

    Update #313 Super Ugly Number.java

--- #313 Super Ugly Number.java
+++ #313 Super Ugly Number.java
@@ -1,4 +1,28 @@
 /*
+  Others` Work.
+*/
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int len = primes.length;  
+        int[] res = new int[n];  
+        int[] index = new int[len];  
+        res[0] =1;  
+        if(n<=1) return res[0];  
+        for(int i=1;i<n;i++) {  
+            int num = Integer.MAX_VALUE;  
+            for(int j=0;j<len;j++) {  
+                num = Math.min(num, primes[j] * res[index[j]]);  
+            }  
+            res[i] = num;  
+            for(int j=0;j<len;j++) {  
+                if(res[i] % primes[j]==0) index[j]++;  
+            }  
+        }  
+        return res[n-1];  
+    }
+}
+
+/*
   Time Limit Exceed.
 */
 
