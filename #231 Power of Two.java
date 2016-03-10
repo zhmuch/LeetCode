4a6adf2836edd434f685d921b53b8579cb284ed1
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-03-10 01:31:47 -0500

    Create #231 Power of Two.java

---
+++ #231 Power of Two.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(n < 1)
+            return false;
+        
+        while(n != 1){
+            if ((n & 1) == 1)
+                return false;
+            n >>= 1;
+        }
+        return true;
+        
+    }
+}
