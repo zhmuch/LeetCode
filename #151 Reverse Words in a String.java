e284ed831ed4c25364e2e6c7f075b4243424666e
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-03-16 01:41:57 -0400

    Update #151 Reverse Words in a String.java

--- #151 Reverse Words in a String.java
+++ #151 Reverse Words in a String.java
@@ -1,4 +1,33 @@
 /*
+  6ms
+*/
+public class Solution {
+  public static String reverseWords(String s) {
+    StringBuilder res = new StringBuilder();
+    for (int start = s.length() - 1; start >= 0; start--) {
+      if (s.charAt(start) == ' ') 
+        continue;
+      int end = start;
+      while (start >= 0 && s.charAt(start) != ' ') 
+        start--;
+      res.append(s.substring(start + 1, end + 1)).append(" ");
+    }
+    return res.toString().trim();
+  }
+}
+
+/*
+  Built-in Library
+*/
+public class Solution {
+  public String reverseWords(String s) {
+      String[] words = s.trim().split(" +");
+      Collections.reverse(Arrays.asList(words));
+      return String.join(" ", words);
+  }
+}
+
+/*
   54ms.
 */
 
