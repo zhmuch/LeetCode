b5a0274df7fed3bd715db1792d2a1f4461bc1360
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-03-21 02:24:53 -0400

    Update #097 Interleaving String.java

--- #097 Interleaving String.java
+++ #097 Interleaving String.java
@@ -1,5 +1,32 @@
 /*
-  2-D DP.
+  Simplized Code for 2D DP.
+*/
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
+        if(l1 + l2 != l3)
+            return false;
+
+        boolean[][] tab = new boolean[l1+1][l2+1];
+
+        tab[0][0] = true;
+        for(int j=1; j<=l2; j++)
+            tab[0][j] = s2.charAt(j-1) == s3.charAt(j-1) && tab[0][j-1];
+
+        for(int i=1; i<=l1; i++){
+            tab[i][0] = s1.charAt(i-1) == s3.charAt(i-1) && tab[i-1][0];
+
+            for(int j=1; j<=l2; j++)
+                tab[i][j] = (s1.charAt(i - 1) == s3.charAt(i + j - 1) && tab[i - 1][j]) || (s2.charAt(j - 1) == s3.charAt(i + j - 1) && tab[i][j-1]);
+            
+        }
+
+        return tab[l1][l2];
+    }
+}
+
+/*
+  2D DP.
 */
 import java.util.*;
 
