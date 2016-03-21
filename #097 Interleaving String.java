d8585dda817e4a751c97cc1e72677374eea8203d
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-03-21 02:17:59 -0400

    Update #097 Interleaving String.java

--- #097 Interleaving String.java
+++ #097 Interleaving String.java
@@ -1,4 +1,45 @@
 /*
+  2-D DP.
+*/
+import java.util.*;
+
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
+        if(l1 + l2 != l3)
+            return false;
+
+        boolean[][] tab = new boolean[l1+1][l2+1];
+
+        tab[0][0] = true;
+        for(int j=1; j<=l2; j++)
+            if(s2.charAt(j-1) == s3.charAt(j-1) && tab[0][j-1])
+                tab[0][j] = true;
+            else
+                tab[0][j] = false;
+
+        for(int i=1; i<=l1; i++){
+            if(s1.charAt(i-1) == s3.charAt(i-1) && tab[i-1][0])
+                tab[i][0] = true;
+            else
+                tab[i][0] = false;
+
+            for(int j=1; j<=l2; j++)
+                if(s1.charAt(i-1) != s3.charAt(i+j-1) && s2.charAt(j-1) != s3.charAt(i+j-1))
+                    tab[i][j] = false;
+                else if (s1.charAt(i-1) == s3.charAt(i+j-1) && s2.charAt(j-1) == s3.charAt(i+j-1))
+                    tab[i][j] = tab[i-1][j] || tab[i][j-1];
+                else if (s1.charAt(i-1) == s3.charAt(i+j-1))
+                    tab[i][j] = tab[i-1][j];
+                else
+                    tab[i][j] = tab[i][j-1];
+        }
+
+        return tab[l1][l2];
+    }
+}
+
+/*
   Recursive top-down. Without DP.
   Time Limit Exceeds.
 */
