d4a175f01715894eb229e99e420d9938dd157063
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-03-21 15:46:31 -0400

    Update #038 Count and Say.java

--- #038 Count and Say.java
+++ #038 Count and Say.java
@@ -1,4 +1,31 @@
 /*
+  Use String Buffer instead of String.
+*/
+public class Solution {
+    public String countAndSay(int n) {
+        if(n < 1)
+            return "";
+
+        String res = "1";
+
+        for(int i=1; i<n; i++){
+            StringBuffer buffer = new StringBuffer();
+            for(int j=0; j<res.length(); ){
+                int count = 1;
+                while( j + count < res.length() && res.charAt(j + count) == res.charAt(j) )
+                    count++;
+                buffer.append(count);
+                buffer.append(res.charAt(j));
+                j += count;
+            }
+            res = buffer.toString();
+        }
+
+        return res;
+    }
+}
+
+/*
   Straightforward.
 */
 
