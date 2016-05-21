da60019472413e313fe160f0662196c268bc95d3
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-21 00:30:45 -0400

    Update #145 Binary Tree Postorder Traversal.java

--- #145 Binary Tree Postorder Traversal.java
+++ #145 Binary Tree Postorder Traversal.java
@@ -1,3 +1,32 @@
+//  Iteratively;    
+//  Tricky one: using preorder traversal;
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> res = new LinkedList<>();
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode tmp = stack.pop();
+            if(tmp == null)
+                continue;
+            else{
+                res.add(tmp.val);
+                stack.push(tmp.left);
+                stack.push(tmp.right);
+            }
+        }
+        
+        List<Integer> rev = new LinkedList<>();
+        for(int i : res)
+            rev.add(0, i);
+            
+        return rev;
+    }
+}
+
 //  Recursively;
 public class Solution {
     
