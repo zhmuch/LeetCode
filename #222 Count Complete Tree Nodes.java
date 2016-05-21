dbf7fdcdba56794163603b29483ae8c3a0690560
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-21 15:57:14 -0400

    Update #222 Count Complete Tree Nodes.java

--- #222 Count Complete Tree Nodes.java
+++ #222 Count Complete Tree Nodes.java
@@ -1,3 +1,31 @@
+//  Little improvement;
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null)
+            return 0;
+            
+        int leftH = 1;
+        TreeNode tmpL = root;
+        while(tmpL.left != null){
+            leftH++;
+            tmpL = tmpL.left;
+        }
+        
+        int rightH = 1;
+        TreeNode tmpR = root;
+        while(tmpR.right != null){
+            rightH++;
+            tmpR = tmpR.right;
+        }
+        
+        if(leftH == rightH)
+            return ((int)Math.pow(2, leftH)) - 1;
+            //  return (2 << (leftH - 1)) - 1;
+        else
+            return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
+
 //  Native;
 public class Solution {
     public int countNodes(TreeNode root) {
