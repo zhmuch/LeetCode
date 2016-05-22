04cf746257c5ec06efd2633601de6ed05d152a5e
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-22 01:11:56 -0400

    Update #297 Serialize and Deserialize Binary Tree.java

--- #297 Serialize and Deserialize Binary Tree.java
+++ #297 Serialize and Deserialize Binary Tree.java
@@ -84,3 +84,93 @@ public class Codec {
         return root;
     }
 }
+
+
+//  Other`s 
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return firstRoot(root);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.equals("")) return null;
+        String [] fs = data.split(",");
+        Stack<TreeNode> space = new Stack<TreeNode>();
+        int state = 0;
+        TreeNode root = null;
+        for (int i = 0; i < fs.length; i++) {
+            TreeNode node = null;
+            if (state == 0) {
+                if (!fs[i].equals("#")) {
+                    node = new TreeNode(Integer.parseInt(fs[i]));
+                    if (!space.isEmpty()) {
+                        TreeNode lastNode = space.peek();
+                        lastNode.left = node;
+                    }
+                    space.push(node);
+                } else {
+                    state = 1;
+                }
+            } else if (state == 1) {
+                if (!fs[i].equals("#")) {
+                    node = new TreeNode(Integer.parseInt(fs[i]));
+                    if (!space.isEmpty()) {
+                        TreeNode lastNode = space.peek();
+                        lastNode.right = node;
+                    }
+                    state = 0;
+                    space.push(node);
+                } else {
+                    TreeNode lastNode = space.pop();
+                    while (!space.isEmpty() && space.peek().right == lastNode) {
+                        lastNode = space.pop();
+                    }
+                }
+            }
+            if (root == null) root = node;
+        }
+        return root;
+    }
+
+    public String firstRoot(TreeNode root) {
+        if (root == null) return "";
+        StringBuffer sb = new StringBuffer("");
+        Stack<TreeNode> space = new Stack<TreeNode>();
+        space.push(root);
+        while (!space.isEmpty()) {
+            TreeNode node = space.pop();
+            if (node.val != Integer.MIN_VALUE) {
+                sb.append(sb.length() == 0 ? node.val : "," + node.val);
+                if (node.right != null) {
+                    space.push(node.right);
+                } else {
+                    space.push(new TreeNode(Integer.MIN_VALUE));
+                }
+
+                if (node.left != null) {
+                    space.push(node.left);
+                } else {
+                    space.push(new TreeNode(Integer.MIN_VALUE));
+                }
+            } else {
+                sb.append(sb.length() == 0 ? "#" : ",#");
+            }
+        }
+        return sb.toString();
+    }
+
+    public void firstRoot(TreeNode root, int sign) {
+        if (root == null) return;
+        Stack<TreeNode> space = new Stack<TreeNode>();
+        space.push(root);
+        while (!space.isEmpty()) {
+            TreeNode node = space.pop();
+            System.out.println(node.val);
+            if (node.right != null) space.push(node.right);
+            if (node.left != null) space.push(node.left);
+        }
+    }
+}
