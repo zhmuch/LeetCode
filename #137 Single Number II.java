07a9322c34ce53440fa04d9a0c13593f31e74326
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-26 17:04:49 -0400

    Create #137 Single Number II.java

---
+++ #137 Single Number II.java
@@ -0,0 +1,16 @@
+//  !!
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int one = 0, two = 0, three = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            two |= one & nums[i];
+            one ^= nums[i];
+            three = one & two;
+            one &= ~three;
+            two &= ~three;
+        }
+
+        return one;
+    }
+}
