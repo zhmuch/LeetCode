4034658e1835487ff1f375691eabde80b7d0d0b5
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-26 21:15:49 -0400

    Update #190 Reverse Bits.java

--- #190 Reverse Bits.java
+++ #190 Reverse Bits.java
@@ -1,6 +1,18 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
+        int i,bin=0;
+        for (i = 0; i < 32; i++)
+            bin+=(n >> i & 1)<<(31-i);
+        return bin;
+    }
+}
+
+
+//  Simple,
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
          return Integer.reverse(n);
     }
 }
