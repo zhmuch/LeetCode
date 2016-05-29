7fdac89daac5e722d51bf6524ae857f0ba1b0994
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-29 14:58:50 -0400

    Update #044 Wildcard Matching.java

--- #044 Wildcard Matching.java
+++ #044 Wildcard Matching.java
@@ -1,3 +1,31 @@
+//  Bloody fast, other`s;
+public class Solution {
+public boolean isMatch(String s, String p) {
+        char p_ar[] = p.toCharArray();
+        char s_ar[] = s.toCharArray();
+        int save_i, save_j, i, j;
+        save_i = save_j = -1;
+        i = j = 0;
+        while(i < s_ar.length){
+            if(j < p_ar.length && (s_ar[i] == p_ar[j] || p_ar[j] == '?')){
+                ++i;
+                ++j;
+            }else if(j < p_ar.length && p_ar[j] == '*'){
+                save_i = i + 1;
+                save_j = j;
+                ++j;
+            }else if(save_i != -1){
+                i = save_i;
+                j = save_j;
+            }else return false;
+        }
+        while(j < p_ar.length)if(p_ar[j++] != '*')return false;
+        return i == s_ar.length && j == p_ar.length;
+    }
+
+}
+
+
 /*
   Greedy Algorithm.
 */
