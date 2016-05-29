80b7977ca268b618b0baf923db5f420c9cd8a552
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-29 02:49:20 -0400

    Update #310 Minimum Height Trees.java

--- #310 Minimum Height Trees.java
+++ #310 Minimum Height Trees.java
@@ -1,3 +1,46 @@
+//  Other`s;
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> leaves = new ArrayList<>();
+        if(n <= 1) {
+            return Collections.singletonList(0);
+        }
+        Map<Integer, Set<Integer>> graph = new HashMap<>();     // list of edges to  Ajacency Lists
+        
+        for(int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<Integer>());
+        }
+        for(int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(graph.get(i).size() == 1) {
+                leaves.add(i);
+            }
+        }
+        
+        while(n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for(int leaf : leaves) {
+                for(int newLeaf : graph.get(leaf)) {
+                    graph.get(leaf).remove(newLeaf);
+                    graph.get(newLeaf).remove(leaf);
+                    if(graph.get(newLeaf).size() == 1) {
+                        newLeaves.add(newLeaf);
+                    }
+                }
+            }
+            leaves = newLeaves;
+        }
+        
+        return leaves;
+    }
+}
+
+
 //  Bellman-Ford Algorithm;
 //  Time Limit Exceeds;
 import java.util.*;
