d7596ae082c4a9470b8d67b65d0f9fdb739f3510
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-29 02:46:10 -0400

    Create #310 Minimum Height Trees.java

---
+++ #310 Minimum Height Trees.java
@@ -0,0 +1,61 @@
+//  Bellman-Ford Algorithm;
+//  Time Limit Exceeds;
+import java.util.*;
+
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+
+        //  Initialization;
+        int[][] edge = new int[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                edge[i][j] = Integer.MAX_VALUE / 2;
+        for(int i = 0; i < n; i++)
+            edge[i][i] = 0;
+
+        for(int[] i : edges){
+            edge[i[0]][i[1]] = 1;
+            edge[i[1]][i[0]] = 1;
+        }
+
+        //  Bellman-Ford
+        int m = 1;
+        while(m < n - 1){
+            int[][] newEdge = new int[n][n];
+
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < n; j++){
+                    newEdge[i][j] = Integer.MAX_VALUE / 2;
+                    for(int k = 0; k < n; k++)
+                        newEdge[i][j] = Math.min(newEdge[i][j], edge[i][k] + edge[k][j]);
+                }
+
+            edge = newEdge;
+            m *= 2;
+        }
+
+//        for(int i = 0; i < n; i++){
+//            for(int j = 0; j < n; j++)
+//                System.out.print(edge[i][j] + " ");
+//            System.out.println();
+//        }
+
+        List<Integer> res = new LinkedList<>();
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++){
+            int max = 0;
+            for(int j = 0; j < n; j++)
+                max = Math.max(max, edge[i][j]);
+
+            if(max < min){
+                res.clear();
+                res.add(i);
+                min = max;
+            } else if(max == min)
+                res.add(i);
+        }
+
+        return res;
+    }
+}
