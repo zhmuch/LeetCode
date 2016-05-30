147d8ce606c942828b529931c92f6b9cc804b0c1
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-30 17:17:40 -0400

    Update #212 Word Search II.java

--- #212 Word Search II.java
+++ #212 Word Search II.java
@@ -1,3 +1,125 @@
+//  A little improvement;
+import java.util.*;
+
+public class Solution {
+    List<String> res;
+    int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    char[][] board;
+    Trie dict;
+    int row, col;
+    public List<String> findWords(char[][] board, String[] words) {
+
+        res = new LinkedList<>();
+        if(board == null)
+            return res;
+        row = board.length;
+        if(row < 1)
+            return res;
+        col = board[0].length;
+        if(col < 1)
+            return res;
+
+        this.board = board;
+
+        dict = new Trie();
+        for(String s : words)
+            dict.insert(s);
+
+        //  Special Case;
+        if(dict.root.isWord)
+            res.add("");
+
+        for(int i = 0; i < row; i++)
+            for(int j = 0; j < col; j++){
+                TrieNode node = dict.root;
+                if(node.links[board[i][j] - 'a'] != null){
+                    StringBuilder s = new StringBuilder();
+                    s.append(board[i][j]);
+                    node = node.links[board[i][j] - 'a'];
+                    dfs(node, s, i, j, -1);
+                }
+            }
+
+        return res;
+
+    }
+
+    private void dfs(TrieNode node, StringBuilder curr, int row, int col, int direct) {
+        String tmp = curr.toString();
+//        System.out.println(tmp);
+        if(node.isWord){
+            node.isWord = false;
+            res.add(tmp);
+        }
+
+        for(int i = 0; i < 4; i++)
+            if(i != 3 - direct){
+                int nextRow = row + directions[i][0];
+                int nextCol = col + directions[i][1];
+
+                if(nextRow >= 0 && nextRow < this.row && nextCol >= 0 && nextCol < this.col){
+                    char c = board[nextRow][nextCol];
+                    if(node.links[c - 'a'] != null){
+                        curr.append(c);
+                        dfs(node.links[c - 'a'], curr, nextRow, nextCol, i);
+                        curr.deleteCharAt(curr.length() - 1);
+                    }
+                }
+            }
+    }
+
+    class TrieNode {
+        // Initialize your data structure here.
+        char c;
+        boolean isWord;
+
+        TrieNode[] links = new TrieNode[26];
+
+        public TrieNode() {}
+
+        public TrieNode(char c){
+            this.c = c;
+        }
+    }
+
+    public class Trie {
+        public TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        // Inserts a word into the trie.
+        public void insert(String word) {
+            if(word == null || word.length() < 1){
+                root.isWord = true;
+                return;
+            }
+
+            TrieNode curr = root;
+            int idx = 0;
+
+            while(idx < word.length()){
+                char c = word.charAt(idx);
+                if(curr.links[c - 'a'] != null)
+                    curr = curr.links[c - 'a'];
+                else {
+                    TrieNode tmp = new TrieNode(c);
+                    curr.links[c - 'a'] = tmp;
+                    curr = tmp;
+                }
+
+                idx++;
+            }
+            curr.isWord = true;
+        }
+    }
+}
+
+
+
+
+
 //  Lots of redundant;
 import java.util.*;
 
