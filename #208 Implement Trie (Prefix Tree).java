83661880bbab5508a2ee92a641d859f5d71740fb
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-30 15:54:47 -0400

    Update #208 Implement Trie (Prefix Tree).java

--- #208 Implement Trie (Prefix Tree).java
+++ #208 Implement Trie (Prefix Tree).java
@@ -1,8 +1,118 @@
-import java.util.Hashtable;
+/**
+ * Object[] implementation;
+ */
+
+class TrieNode {
+    // Initialize your data structure here.
+    char c;
+    boolean isWord;
+    boolean added;
+
+    TrieNode[] links = new TrieNode[26];
+
+    public TrieNode() {}
+
+    public TrieNode(char c){
+        this.c = c;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        if(word == null || word.length() < 1)
+            root.isWord = true;
+
+        TrieNode curr = root;
+        int idx = 0;
+
+        while(idx < word.length()){
+            char c = word.charAt(idx);
+            if(curr.links[c - 'a'] != null)
+                curr = curr.links[c - 'a'];
+            else {
+                TrieNode tmp = new TrieNode(c);
+                curr.links[c - 'a'] = tmp;
+                curr = tmp;
+            }
+
+            idx++;
+        }
+        curr.isWord = true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        if(word == null || word.length() < 1)
+            return root.isWord;
+        else{
+            TrieNode curr = root;
+            int idx = 0;
+
+            while(idx < word.length()){
+                char c = word.charAt(idx);
+                if(curr.links[c - 'a'] != null)
+                    curr = curr.links[c - 'a'];
+                else
+                    return false;
+                idx++;
+            }
+
+            return curr.isWord;
+        }
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        if(prefix == null || prefix.length() < 1){
+            boolean ext = false;
+            for(int i = 0; i < 26; i++){
+                ext = ext || root.links[i] != null;
+                if(ext)
+                    break;
+            }
+            return ext;
+        }
+        else{
+            TrieNode curr = root;
+            int idx = 0;
+
+            while(idx < prefix.length()){
+                char c = prefix.charAt(idx);
+                if(curr.links[c - 'a'] != null)
+                    curr = curr.links[c - 'a'];
+                else
+                    return false;
+                idx++;
+            }
+
+            boolean ext = false;
+            for(int i = 0; i < 26; i++){
+                ext = ext || curr.links[i] != null;
+                if(ext)
+                    break;
+            }
+            return curr.isWord || ext;
+        }
+    }
+}
+
+
 
 /**
- * Created by Muchen on 5/30/16.
+ * HashTable implemention;
  */
+
+  
+import java.util.Hashtable;
+
 class TrieNode {
     // Initialize your data structure here.
     char c;
