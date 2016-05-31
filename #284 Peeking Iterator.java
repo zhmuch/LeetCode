3fc5fdbb8d96a1867d664642b95b67149edca5f1
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-05-31 15:47:40 -0400

    Create #284 Peeking Iterator.java

---
+++ #284 Peeking Iterator.java
@@ -0,0 +1,46 @@
+import java.util.Iterator;
+
+/**
+ * Created by Muchen on 5/31/16.
+ */
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+class PeekingIterator implements Iterator<Integer> {
+
+    Iterator<Integer> iterator;
+    boolean flag;
+    int tmp;
+
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        this.iterator = iterator;
+        flag = false;
+        tmp = 0;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        if(!flag){
+            tmp = next();
+            flag = true;
+        }
+        return tmp;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        if(flag){
+            flag = false;
+            return tmp;
+        } else
+            return iterator.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return flag || iterator.hasNext();
+    }
+}
