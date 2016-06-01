2294ccdc955db907e6fd76a125586eda50db7cfa
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-01 15:16:13 -0400

    Update #134 Gas Station.java

--- #134 Gas Station.java
+++ #134 Gas Station.java
@@ -1,3 +1,47 @@
+//  Greedy;
+import java.util.*;
+
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if(gas == null || cost == null || gas.length != cost.length || gas.length < 1)
+            return -1;
+
+        int res = -1;
+        int i = 0;
+
+        while(i < gas.length) {
+            int tank = 0;
+            int idx = i;
+            boolean flag = false;
+
+            while(true) {
+                tank += gas[idx] - cost[idx];
+                if(tank < 0)
+                    break;
+
+                if(idx == gas.length - 1){
+                    flag = true;
+                    idx = 0;
+                } else
+                    idx++;
+
+                if(idx == i){
+                    return i;
+                }
+            }
+
+            if(flag)
+                break;
+
+            i = idx + 1;
+        }
+
+        return res;
+    }
+}
+
+
+
 //  Naive Search;
 //  TLE;
 
