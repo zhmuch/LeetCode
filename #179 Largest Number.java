1b40dfbf82180378a169d70f8dcf87f7d5c45388
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-02 15:59:42 -0400

    Update #179 Largest Number.java

--- #179 Largest Number.java
+++ #179 Largest Number.java
@@ -3,8 +3,6 @@
 
 import java.util.*;
 
-//  Radix Sort;
-
 public class Solution {
     public String largestNumber(int[] nums) {
         if(nums == null || nums.length < 1)
@@ -14,16 +12,9 @@ public class Solution {
         for(int i : nums)
             lists.add(convertToList(i));
 
-//        for(List<Integer> i : lists)
-//            System.out.println(i);
-
         Comparator<List<Integer>> comparator = new Comparator<List<Integer>>() {
             @Override
             public int compare(List<Integer> num1, List<Integer> num2) {
-
-//                System.out.println("num1 " + num1);
-//                System.out.println("num2 " + num2);
-
                 List<Integer> tmp1 = new ArrayList<>();
                 List<Integer> tmp2 = new ArrayList<>();
 
@@ -42,50 +33,10 @@ public class Solution {
                         return tmp2.get(idx) - tmp1.get(idx);
 
                 return 0;
-
-//                if(num1.get(0) != num2.get(0))
-//                    return num2.get(0) - num1.get(0);
-//                else{
-//                    int first = num1.get(0);
-//                    int idx = 1;
-//
-//                    while(num1.size() > idx && num2.size() > idx){
-//                        if(num1.get(idx) != num2.get(idx))
-//                            return num2.get(idx) - num1.get(idx);
-//                        else
-//                            idx++;
-//                    }
-//
-//                    System.out.println("idx: " + idx);
-//                    System.out.println("first: " + first);
-//
-//                    if(num1.size() == num2.size())
-//                        return 0;
-//                    else if(num1.size() > num2.size()){
-//                        while(num1.size() > idx && num1.get(idx) == first)
-//                            idx++;
-//                        if(num1.size() == idx)
-//                            return 0;
-//                        else
-//                            return first - num1.get(idx);
-//                    } else {
-//                        while (num2.size() > idx && num2.get(idx) == first)
-//                            idx++;
-//                        if (num2.size() == idx)
-//                            return 0;
-//                        else
-//                            return num2.get(idx) - first;
-//                    }
-//                }
             }
         };
 
         Collections.sort(lists, comparator);
-
-//        System.out.println("After Sort: ");
-//        for(List<Integer> i : lists)
-//            System.out.println(i);
-
 
         StringBuilder s = new StringBuilder();
         for(List<Integer> i : lists)
@@ -108,9 +59,19 @@ public class Solution {
                 n /= 10;
             }
         }
-
-//        System.out.println(res);
-
         return res;
     }
 }
+      
+
+/**
+ *  另外的做法
+ *  Radix sort！！
+ * 
+ * 
+ */ 
+...
+...
+...
+...
+...
