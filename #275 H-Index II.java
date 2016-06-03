06aa6af434cb9f269c6f1b47b5cd3267e132ca93
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-03 00:38:28 -0400

    Update #275 H-Index II.java

--- #275 H-Index II.java
+++ #275 H-Index II.java
@@ -1,3 +1,32 @@
+//  Simple Binary Search;
+import java.util.*;
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length < 1)
+            return 0;
+
+        int len = citations.length;
+        int left = 0, right = len - 1;
+
+        while(left < right){
+            int med = (left + right) / 2;
+
+            if(len - med == citations[med])
+                return len - med;
+            else if(len - med > citations[med])
+                left = med + 1;
+            else
+                right = med;
+        }
+
+        int med = (left + right) / 2;
+        return Math.min(len - med, citations[med]); 
+    }
+}
+
+
+
 //  Naive Solution;
 //  Same as #274;
 //  O(n);
