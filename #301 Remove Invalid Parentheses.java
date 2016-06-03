efdb1eb64980b931ff618d0ed78ff83bced2868b
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-03 15:00:33 -0400

    Update #301 Remove Invalid Parentheses.java

--- #301 Remove Invalid Parentheses.java
+++ #301 Remove Invalid Parentheses.java
@@ -1,3 +1,33 @@
+//  Other`s;
+//  Bloody fast!!!!!
+
+public class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> res = new ArrayList<>();
+        dfs(s, res, 0, 0, new char[]{'(',')'});
+        return res;
+    }
+    public void dfs(String s,List<String> res, int starti, int startj, char[] par){
+        char[] ar = s.toCharArray();
+        for(int num = 0, i = starti; i < ar.length; ++i){
+            if(ar[i] == par[0])++num;
+            if(ar[i] == par[1])--num;
+            if(num >= 0)continue;
+            for(int j = startj; j <= i; ++j)
+                if(ar[j] == par[1] && (j == startj || ar[j - 1] != par[1]))
+                    dfs(s.substring(0, j) + s.substring(j + 1, ar.length), res, i, j, par);
+            return;
+        }
+        String reversed = new StringBuilder(s).reverse().toString();
+        if(par[0] == '(')
+            dfs(reversed,res, 0, 0, new char[] {')','('});
+        else
+            res.add(reversed);
+    }
+}
+
+
+
 //  Unfinished;
 
 import java.util.*;
