78cdf20159b636cc7c6c604d57eedb161fed0374
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-05 15:38:11 -0400

    Update #331 Verify Preorder Serialization of a Binary Tree.java

--- #331 Verify Preorder Serialization of a Binary Tree.java
+++ #331 Verify Preorder Serialization of a Binary Tree.java
@@ -25,3 +25,42 @@ public class Solution {
         return leaves == 0;
     }
 }
+
+
+//  A little faster;
+
+import java.util.*;
+
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        if(preorder == null || preorder.length() < 1)
+            return false;
+
+        int leaves = 2;
+        if(preorder.charAt(0) == '#')
+            return preorder.length() == 1;
+
+        boolean prev = false;
+        
+        for(int i = 1; i < preorder.length(); i++){
+            char c = preorder.charAt(i);
+            if(c == ','){
+                prev = false;
+                continue;
+            }
+            else if(c == '#'){
+                prev = false;
+                leaves--;
+            }
+            else if(!prev){
+                leaves++;
+                prev = true;
+            }
+
+            if(leaves == 0)
+                return i == preorder.length() - 1;
+        }
+
+        return false;
+    }
+}
