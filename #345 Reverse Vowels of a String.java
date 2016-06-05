0528594388db68a4aab0248d05f29171e2b2af22
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-05 16:42:14 -0400

    Update #345 Reverse Vowels of a String.java

--- #345 Reverse Vowels of a String.java
+++ #345 Reverse Vowels of a String.java
@@ -1,3 +1,31 @@
+//  Faster;
+public class Solution {
+    public String reverseVowels(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException();
+        }
+        if (s.length() <= 1) {
+            return s;
+        }
+        char[] str = s.toCharArray();
+        for (int i = 0, j = str.length - 1; i < j; i++, j--) {
+            while (i < j && !isVowel(str[i])) i++;
+            while (i < j && !isVowel(str[j])) j--;
+            if (i <= j) swap(str, i, j);
+        }
+        return new String(str);
+    }
+    private boolean isVowel(char ch) {
+        char c = Character.toLowerCase(ch);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+    private void swap(char[] str, int i, int j) {
+        char tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+    }
+}
+
 //  Standard solution;
 import java.util.*;
 
