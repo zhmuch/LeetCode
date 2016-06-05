4628430a441ff011c27cfd65138b50a48080878b
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-05 16:13:42 -0400

    Update #334 Increasing Triplet Subsequence.java

--- #334 Increasing Triplet Subsequence.java
+++ #334 Increasing Triplet Subsequence.java
@@ -25,3 +25,36 @@ public class Solution {
         return false;
     }
 }
+
+//  Simple;
+import java.util.*;
+
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums == null || nums.length < 3)
+            return false;
+
+        int[] tri = new int[3];
+        tri[0] = nums[0];
+        int len = 1;
+
+        for(int x : nums){
+            
+            int i = 0;
+            while(i < len && tri[i] < x)
+                i++;
+
+            if(i < len && tri[i] == x)
+                continue;
+            
+            if(i == len)
+                len++;
+
+            tri[i] = x;
+            
+            if(len == 3)
+                return true;
+        }
+        return false;
+    }
+}
